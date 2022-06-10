5cbe5e9300c97255e54f6bc0e732b14dc1d9a95f
Jennan Wang <zawang727@gapp.nthu.edu.tw>
2022-06-10 08:42:25 +0800

    20220610_0

--- fileDiffSolverByLine.cpp
+++ fileDiffSolverByLine.cpp
@@ -4,5 +4,36 @@ diffInfo fileDiffSolverByLine::solve()
 {
     diffInfo resDiff;
     resDiff.firstFileName = this->firstFileName;
+    resDiff.firstFolderName = this->firstFolderName;
+    resDiff.secondFileName = this->secondFileName;
+    resDiff.secondFolderName = this->secondFolderName;
+
+    bool inDiff = false;
+    int startDiff = 1;
+    unsigned int compareLines = (firstContents.size()>secondContents.size())? 
+    secondContents.size():firstContents.size();
+
+    for(int i = 0; i < compareLines-1; ++i)
+    {
+        if(firstContents[i+1] != secondContents[i+1])
+        {
+            if(inDiff)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            if(inDiff)
+            {
+                resDiff.firstFileDiff.insert(pair(-1*(int)startDiff, i),firstContents);
+            }
+        }
+    }
+
     return resDiff;
 }
